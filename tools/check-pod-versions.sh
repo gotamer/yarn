a24f1536ccc42513b8e26ed9a1e6432d0f0205eb
James Mills <prologic@shortcircuit.net.au>
2021-10-22 21:20:38 +1000

    Add shell script to check versions of known pods

---
+++ tools/check-pod-versions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+pods="twtxt.net txt.sour.is twt.nfld.uk tt.vlra.plus f.adi.onl yarn.andrewjvpowell.com we.loveprivacy.club"
+
+printf "Pod Version\n"
+
+for pod in $pods; do
+  printf "%s " "$pod"
+  if ! curl -fqso - -H 'Accept: application/json' "https://${pod}/version" |
+    jq -er '.FullVersion'; then
+    printf "???\n"
+  fi
+done
