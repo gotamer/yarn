gotamer/yarn

commit e9222f5ed476d625f960baa77a8ee50ade331fde

Add immediate on-pod feed/persona updates

internal/api.go
@@ -387,11 +387,22 @@ func (a *API) PostEndpoint() httprouter.Handle {
 			return
 		}
 
+		var sources types.Feeds
+
 		switch req.PostAs {
 		case "", me:
+			sources = user.Sources()
 			_, err = AppendTwt(a.config, a.db, user, text)
 		default:
 			if user.OwnsFeed(req.PostAs) {
+				if feed, err := a.db.GetFeed(req.PostAs); err == nil {
+					sources = feed.Source()
+				} else {
+					log.WithError(err).Error("error posting twt")
+					http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+					return
+				}
+
 				_, err = AppendSpecial(a.config, a.db, req.PostAs, text)
 			} else {
 				err = ErrFeedImposter
@@ -409,7 +420,7 @@ func (a *API) PostEndpoint() httprouter.Handle {
 		}
 
 		// Update user's own timeline with their own new post.
-		a.cache.FetchTwts(a.config, a.archive, user.Source(), nil)
+		a.cache.FetchTwts(a.config, a.archive, sources, nil)
 
 		// Re-populate/Warm cache with local twts for this pod
 		a.cache.GetByPrefix(a.config.BaseURL, true)

internal/handlers.go
@@ -596,10 +596,13 @@ func (s *Server) PostHandler() httprouter.Handle {
 			return
 		}
 
+		var sources types.Feeds
 		var twt types.Twt = types.NilTwt
 
 		switch postas {
 		case "", user.Username:
+			sources = user.Sources()
+
 			if hash != "" && lastTwt.Hash() == hash {
 				twt, err = AppendTwt(s.config, s.db, user, text, lastTwt.Created())
 			} else {
@@ -607,6 +610,16 @@ func (s *Server) PostHandler() httprouter.Handle {
 			}
 		default:
 			if user.OwnsFeed(postas) {
+				if feed, err := s.db.GetFeed(postas); err == nil {
+					sources = feed.Source()
+				} else {
+					log.WithError(err).Error("error loading feed object")
+					ctx.Error = true
+					ctx.Message = s.tr(ctx, "ErrorPostingTwt")
+					s.render("error", w, ctx)
+					return
+				}
+
 				if hash != "" && lastTwt.Hash() == hash {
 					twt, err = AppendSpecial(s.config, s.db, postas, text, lastTwt.Created)
 				} else {
@@ -626,7 +639,7 @@ func (s *Server) PostHandler() httprouter.Handle {
 		}
 
 		// Update user's own timeline with their own new post.
-		s.cache.FetchTwts(s.config, s.archive, user.Source(), nil)
+		s.cache.FetchTwts(s.config, s.archive, sources, nil)
 
 		// Re-populate/Warm cache with local twts for this pod
 		s.cache.GetByPrefix(s.config.BaseURL, true)

commit 10a0c28bfdc6f98dc571ffc58a877a39c6f50506

Fix performance regression posting as user that e9222f5 broke

internal/api.go
@@ -391,7 +391,7 @@ func (a *API) PostEndpoint() httprouter.Handle {
 
 		switch req.PostAs {
 		case "", me:
-			sources = user.Sources()
+			sources = user.Source()
 			_, err = AppendTwt(a.config, a.db, user, text)
 		default:
 			if user.OwnsFeed(req.PostAs) {

internal/handlers.go
@@ -601,7 +601,7 @@ func (s *Server) PostHandler() httprouter.Handle {
 
 		switch postas {
 		case "", user.Username:
-			sources = user.Sources()
+			sources = user.Source()
 
 			if hash != "" && lastTwt.Hash() == hash {
 				twt, err = AppendTwt(s.config, s.db, user, text, lastTwt.Created())