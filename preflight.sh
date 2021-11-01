32fba38cfd8d902e04e2a3a6bfef4a9663a64493
James Mills <prologic@shortcircuit.net.au>
2021-11-02 08:47:04 +1000

    Fix preflight check for Go1.17

--- preflight.sh
+++ preflight.sh
@@ -75,7 +75,7 @@ check_goversion() {
 
   gover="$(go version | grep -o -E 'go[0-9]+\.[0-9]+(\.[0-9]+)?')"
 
-  if ! go version | grep -E 'go1\.17\.[0-9]+' > /dev/null; then
+  if ! go version | grep -E 'go1\.17(\.[0-9]+)?' > /dev/null; then
     log2 "Go 1.17 is required, found ${gover}"
     return 1
   fi
