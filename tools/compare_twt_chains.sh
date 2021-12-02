04a3f578d86e80ecce23f599588ccdc99d97b0ed
James Mills <prologic@shortcircuit.net.au>
2021-12-03 01:01:40 +1000

    Add tools/compare_twt_chains.sh for comparing two pods conversation chains for set difference of twts

---
+++ tools/compare_twt_chains.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+  echo "Usage: $(basename "$0") <source_pod> <target_pod> <conv_hash>"
+  exit 1
+fi
+
+source_pod="$1"
+target_pod="$2"
+conv_hash="$3"
+
+comm -3 \
+  <(curl -qso - -H 'Accept: application/json' "$source_pod/conv/$conv_hash" | jq -r '.[] | .hash' | sort) \
+  <(curl -qso - -H 'Accept: application/json' "$target_pod/conv/$conv_hash" | jq -r '.[] | .hash' | sort)
