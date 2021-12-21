df0e1174f1fc155b7d7300afa247165da5f49a94
James Mills <prologic@shortcircuit.net.au>
2021-12-21 10:14:52 +1000

    Add a better tool (written in Go) for checking pod versions

---
+++ tools/check_pod_versions.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"mime"
+	"net/http"
+	"os"
+	"time"
+)
+
+var pods = []string{
+	"twtxt.net",
+	"twtxt.cc",
+	"txt.sour.is",
+	"twt.nfld.uk",
+	"tt.vltra.plus",
+	"yarn.andrewjvpowell.com",
+	"we.loveprivacy.club",
+	"arrakis.netbros.com",
+	"tw.lohn.in",
+	"yarn.meff.me",
+	"txt.quisquiliae.com",
+}
+
+func request(method, url string, headers http.Header) (*http.Response, error) {
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if headers == nil {
+		headers = make(http.Header)
+	}
+
+	// Set a default User-Agent (if none set)
+	if headers.Get("User-Agent") == "" {
+		headers.Set("User-Agent", "./tools/check_pod_versions.go (https://git.mills.io/yarnsocial/yarn)")
+	}
+
+	req.Header = headers
+
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func makeJsonRequest(url string) ([]byte, error) {
+	headers := make(http.Header)
+	headers.Set("Accept", "application/json")
+
+	res, err := request(http.MethodGet, url, headers)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode/100 != 2 {
+		return nil, fmt.Errorf("non-success HTTP %s response for %s", res.Status, url)
+	}
+
+	if ctype := res.Header.Get("Content-Type"); ctype != "" {
+		mediaType, _, err := mime.ParseMediaType(ctype)
+		if err != nil {
+			return nil, err
+		}
+		if mediaType != "application/json" {
+			return nil, fmt.Errorf("non-JSON response content type '%s' for %s", ctype, url)
+		}
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func getLegacyPodVersion(url string) (string, error) {
+	version := struct {
+		FullVersion string
+	}{
+		FullVersion: "",
+	}
+	data, err := makeJsonRequest(url + "/version")
+	if err != nil {
+		return "", err
+	}
+	if err := json.Unmarshal(data, &version); err != nil {
+		return "", err
+	}
+	return version.FullVersion, nil
+}
+
+func getPodVersion(url string) (string, error) {
+	info := struct {
+		SoftwareVersion string `json:"software_version"`
+	}{
+		SoftwareVersion: "",
+	}
+	data, err := makeJsonRequest(url + "/info")
+	if err != nil {
+		return getLegacyPodVersion(url)
+	}
+	if err := json.Unmarshal(data, &info); err != nil {
+		return getLegacyPodVersion(url)
+	}
+	return info.SoftwareVersion, nil
+}
+
+func main() {
+	fmt.Println("Pod Version\n")
+	for _, pod := range pods {
+		fmt.Printf("%s ", pod)
+		url := fmt.Sprintf("https://%s", pod)
+		version, err := getPodVersion(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error getting pod version for %s: %s\n", pod, err)
+			fmt.Println("???")
+		} else {
+			fmt.Println(version)
+		}
+	}
+}
