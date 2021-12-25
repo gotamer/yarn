5f7a204c112508f4b1388cf54f9b37603ca9dd99
James Mills <prologic@shortcircuit.net.au>
2021-12-25 19:45:02 +1000

    Fix data race in BaseTask

--- internal/base_task.go
+++ internal/base_task.go
@@ -2,11 +2,13 @@ package internal
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/renstrom/shortuuid"
 )
 
 type BaseTask struct {
+	mu    sync.RWMutex
 	state TaskState
 	data  TaskData
 	err   error
@@ -21,30 +23,40 @@ func NewBaseTask() *BaseTask {
 }
 
 func (t *BaseTask) SetState(state TaskState) {
+	t.mu.Lock()
 	t.state = state
+	t.mu.Unlock()
 }
 
 func (t *BaseTask) SetData(key, val string) {
+	t.mu.Lock()
 	if t.data == nil {
 		t.data = make(TaskData)
 	}
 	t.data[key] = val
+	t.mu.Unlock()
 }
 
 func (t *BaseTask) Done() {
 	if t.err != nil {
-		t.state = TaskStateFailed
+		t.SetState(TaskStateFailed)
 	} else {
-		t.state = TaskStateComplete
+		t.SetState(TaskStateComplete)
 	}
 }
 
 func (t *BaseTask) Fail(err error) error {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
 	t.err = err
 	return err
 }
 
 func (t *BaseTask) Result() TaskResult {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+
 	stateStr := t.state.String()
 	errStr := ""
 	if t.err != nil {
@@ -58,7 +70,27 @@ func (t *BaseTask) Result() TaskResult {
 	}
 }
 
-func (t *BaseTask) String() string   { return fmt.Sprintf("%T: %s", t, t.ID()) }
-func (t *BaseTask) ID() string       { return t.id }
-func (t *BaseTask) State() TaskState { return t.state }
-func (t *BaseTask) Error() error     { return t.err }
+func (t *BaseTask) String() string {
+	return fmt.Sprintf("%T: %s", t, t.ID())
+}
+
+func (t *BaseTask) ID() string {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+
+	return t.id
+}
+
+func (t *BaseTask) State() TaskState {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+
+	return t.state
+}
+
+func (t *BaseTask) Error() error {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+
+	return t.err
+}
