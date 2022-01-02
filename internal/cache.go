gotamer/yarn

commit c14cebb0cd26656b5091c1a37d8a6965fd40eef9

Set Profile.LastSeenAt for external profiles to be the Follower.LastSeenAt for the viewing user

internal/cache.go
@@ -1383,6 +1383,22 @@ func (cache *Cache) GetFollowers(profile types.Profile) types.Followers {
 	return followers
 }
 
+// GetFollowerByURI ...
+func (cache *Cache) GetFollowerByURI(user *User, uri string) *types.Follower {
+	cache.mu.RLock()
+	followers := cache.Followers[user.Username]
+	defer cache.mu.RUnlock()
+
+	// TODO: Optimize this to be an O(1) lookup with a new Cached View?
+	for _, f := range followers {
+		if f.URI == uri {
+			return f
+		}
+	}
+
+	return nil
+}
+
 func (cache *Cache) FollowedBy(user *User, uri string) bool {
 	cache.mu.RLock()
 	followers := cache.Followers[user.Username]

internal/external_handlers.go
@@ -125,6 +125,10 @@ func (s *Server) ExternalHandler() httprouter.Handle {
 
 		if len(twts) > 0 {
 			ctx.Profile.LastPostedAt = twts[0].Created()
+		}
+
+		follower := s.cache.GetFollowerByURI(ctx.User, uri)
+		if follower != nil {
 			ctx.Profile.LastSeenAt = twts[0].Created()
 		}
 

internal/handlers.go
@@ -16,8 +16,6 @@ import (
 	"github.com/gorilla/feeds"
 	"github.com/julienschmidt/httprouter"
 	log "github.com/sirupsen/logrus"
-	"github.com/vcraescu/go-paginator"
-	"github.com/vcraescu/go-paginator/adapter"
 	"gopkg.in/yaml.v2"
 
 	"git.mills.io/yarnsocial/yarn/types"
@@ -119,104 +117,6 @@ func (s *Server) UserConfigHandler() httprouter.Handle {
 		}
 
 		_, _ = w.Write(data)
-	}
-}
-
-// ProfileHandler ...
-func (s *Server) ProfileHandler() httprouter.Handle {
-	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		ctx := NewContext(s, r)
-		ctx.Translate(s.translator)
-
-		nick := NormalizeUsername(p.ByName("nick"))
-		if nick == "" {
-			ctx.Error = true
-			ctx.Message = s.tr(ctx, "ErrorNoUser")
-			s.render("error", w, ctx)
-			return
-		}
-
-		var profile types.Profile
-
-		if s.db.HasUser(nick) {
-			user, err := s.db.GetUser(nick)
-			if err != nil {
-				log.WithError(err).Errorf("error loading user object for %s", nick)
-				ctx.Error = true
-				ctx.Message = s.tr(ctx, "ErrorLoadingProfile")
-				s.render("error", w, ctx)
-				return
-			}
-			profile = user.Profile(s.config.BaseURL, ctx.User)
-		} else if s.db.HasFeed(nick) {
-			feed, err := s.db.GetFeed(nick)
-			if err != nil {
-				log.WithError(err).Errorf("error loading feed object for %s", nick)
-				ctx.Error = true
-				ctx.Message = s.tr(ctx, "ErrorLoadingProfile")
-				s.render("error", w, ctx)
-				return
-			}
-			profile = feed.Profile(s.config.BaseURL, ctx.User)
-		} else {
-			ctx.Error = true
-			ctx.Message = s.tr(ctx, "ErrorUserOrFeedNotFound")
-			s.render("404", w, ctx)
-			return
-		}
-
-		followers := s.cache.GetFollowers(profile)
-
-		profile.Followers = followers
-		profile.NFollowers = len(followers)
-
-		profile.FollowedBy = s.cache.FollowedBy(ctx.User, profile.URI)
-
-		ctx.Profile = profile
-
-		ctx.Links = append(ctx.Links, Link{
-			Href: fmt.Sprintf("%s/webmention", UserURL(profile.URI)),
-			Rel:  "webmention",
-		})
-
-		ctx.Alternatives = append(ctx.Alternatives, Alternatives{
-			Alternative{
-				Type:  "text/plain",
-				Title: fmt.Sprintf("%s's Twtxt Feed", profile.Nick),
-				URL:   profile.URI,
-			},
-			Alternative{
-				Type:  "application/atom+xml",
-				Title: fmt.Sprintf("%s's Atom Feed", profile.Nick),
-				URL:   fmt.Sprintf("%s/atom.xml", UserURL(profile.URI)),
-			},
-		}...)
-
-		twts := FilterTwts(ctx.User, s.cache.GetByURL(profile.URI))
-
-		if len(twts) > 0 {
-			profile.LastPostedAt = twts[0].Created()
-		}
-
-		var pagedTwts types.Twts
-
-		page := SafeParseInt(r.FormValue("p"), 1)
-		pager := paginator.New(adapter.NewSliceAdapter(twts), s.config.TwtsPerPage)
-		pager.SetPage(page)
-
-		if err := pager.Results(&pagedTwts); err != nil {
-			log.WithError(err).Error("error sorting and paging twts")
-			ctx.Error = true
-			ctx.Message = s.tr(ctx, "ErrorLoadingTimeline")
-			s.render("error", w, ctx)
-			return
-		}
-
-		ctx.Title = fmt.Sprintf("%s's Profile: %s", profile.Nick, profile.Description)
-		ctx.Twts = pagedTwts
-		ctx.Pager = &pager
-
-		s.render("profile", w, ctx)
 	}
 }
 

internal/profile_handler.go
@@ -1,0 +1,110 @@
+package internal
+
+import (
+	"fmt"
+	"net/http"
+
+	"git.mills.io/yarnsocial/yarn/types"
+	"github.com/julienschmidt/httprouter"
+	log "github.com/sirupsen/logrus"
+	"github.com/vcraescu/go-paginator"
+	"github.com/vcraescu/go-paginator/adapter"
+)
+
+// ProfileHandler ...
+func (s *Server) ProfileHandler() httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		ctx := NewContext(s, r)
+		ctx.Translate(s.translator)
+
+		nick := NormalizeUsername(p.ByName("nick"))
+		if nick == "" {
+			ctx.Error = true
+			ctx.Message = s.tr(ctx, "ErrorNoUser")
+			s.render("error", w, ctx)
+			return
+		}
+
+		var profile types.Profile
+
+		if s.db.HasUser(nick) {
+			user, err := s.db.GetUser(nick)
+			if err != nil {
+				log.WithError(err).Errorf("error loading user object for %s", nick)
+				ctx.Error = true
+				ctx.Message = s.tr(ctx, "ErrorLoadingProfile")
+				s.render("error", w, ctx)
+				return
+			}
+			profile = user.Profile(s.config.BaseURL, ctx.User)
+		} else if s.db.HasFeed(nick) {
+			feed, err := s.db.GetFeed(nick)
+			if err != nil {
+				log.WithError(err).Errorf("error loading feed object for %s", nick)
+				ctx.Error = true
+				ctx.Message = s.tr(ctx, "ErrorLoadingProfile")
+				s.render("error", w, ctx)
+				return
+			}
+			profile = feed.Profile(s.config.BaseURL, ctx.User)
+		} else {
+			ctx.Error = true
+			ctx.Message = s.tr(ctx, "ErrorUserOrFeedNotFound")
+			s.render("404", w, ctx)
+			return
+		}
+
+		followers := s.cache.GetFollowers(profile)
+
+		profile.Followers = followers
+		profile.NFollowers = len(followers)
+
+		profile.FollowedBy = s.cache.FollowedBy(ctx.User, profile.URI)
+
+		ctx.Profile = profile
+
+		ctx.Links = append(ctx.Links, Link{
+			Href: fmt.Sprintf("%s/webmention", UserURL(profile.URI)),
+			Rel:  "webmention",
+		})
+
+		ctx.Alternatives = append(ctx.Alternatives, Alternatives{
+			Alternative{
+				Type:  "text/plain",
+				Title: fmt.Sprintf("%s's Twtxt Feed", profile.Nick),
+				URL:   profile.URI,
+			},
+			Alternative{
+				Type:  "application/atom+xml",
+				Title: fmt.Sprintf("%s's Atom Feed", profile.Nick),
+				URL:   fmt.Sprintf("%s/atom.xml", UserURL(profile.URI)),
+			},
+		}...)
+
+		twts := FilterTwts(ctx.User, s.cache.GetByURL(profile.URI))
+
+		if len(twts) > 0 {
+			profile.LastPostedAt = twts[0].Created()
+		}
+
+		var pagedTwts types.Twts
+
+		page := SafeParseInt(r.FormValue("p"), 1)
+		pager := paginator.New(adapter.NewSliceAdapter(twts), s.config.TwtsPerPage)
+		pager.SetPage(page)
+
+		if err := pager.Results(&pagedTwts); err != nil {
+			log.WithError(err).Error("error sorting and paging twts")
+			ctx.Error = true
+			ctx.Message = s.tr(ctx, "ErrorLoadingTimeline")
+			s.render("error", w, ctx)
+			return
+		}
+
+		ctx.Title = fmt.Sprintf("%s's Profile: %s", profile.Nick, profile.Description)
+		ctx.Twts = pagedTwts
+		ctx.Pager = &pager
+
+		s.render("profile", w, ctx)
+	}
+}

commit 3e946b8c1b31ec23f1bb2bd15000392f3b74f676

Fix stupid  bugs caused by c14cebb

internal/cache.go
@@ -1385,6 +1385,10 @@ func (cache *Cache) GetFollowers(profile types.Profile) types.Followers {
 
 // GetFollowerByURI ...
 func (cache *Cache) GetFollowerByURI(user *User, uri string) *types.Follower {
+	if user == nil || user.IsZero() {
+		return nil
+	}
+
 	cache.mu.RLock()
 	followers := cache.Followers[user.Username]
 	defer cache.mu.RUnlock()

internal/external_handlers.go
@@ -129,7 +129,7 @@ func (s *Server) ExternalHandler() httprouter.Handle {
 
 		follower := s.cache.GetFollowerByURI(ctx.User, uri)
 		if follower != nil {
-			ctx.Profile.LastSeenAt = twts[0].Created()
+			ctx.Profile.LastSeenAt = follower.LastSeenAt
 		}
 
 		trdata := map[string]interface{}{}